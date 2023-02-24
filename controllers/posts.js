a8ef422ea67e6f51b687f0e4fe09222e44a4583a
MassiHadjloum <massihadjloum@gmail.com>
2023-02-24 22:25:14 +0100

    add update function for post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,10 @@
+import mongoose from "mongoose"
 import PostMessage from "../models/postMessage.js"
 
 
 export const getPosts = (req, res) => {
   PostMessage.find().then((response) => {
-    console.log(response)
-    res.status(200).json({ data: response })
+    res.status(200).json({ response })
   })
     .catch((err) => {
       res.status(404).json({ message: err.message })
@@ -21,4 +21,14 @@ export const createPost = (req, res) => {
     .catch((err) => {
       res.status(409).json({ message: err.message })
     })
+}
+
+export const updatePost = (req, res) => {
+  const {id: _id} = req?.params
+  const post = req?.body
+  if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({messag: `No post with id ${_id}`})
+  
+  // add { new: true} to have the updated object from database
+  PostMessage.findByIdAndUpdate(_id, post, {new: true})
+  .then((response) => res?.status(202).json(response))
 }
