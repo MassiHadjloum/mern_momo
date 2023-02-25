8cd3b7675d13ddc691fbd3d48556859105e1d150
MassiHadjloum <massihadjloum@gmail.com>
2023-02-25 03:51:44 +0100

    install dotenv

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ import cors from "cors";
 import mongoose from "mongoose";
 import 'colors';
 import postRouts from './routes/posts.js';
+import dotenv from 'dotenv'
 
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('/posts', postRouts)
 
-const CONNEXION_URL = "mongodb+srv://massi:massi@cluster0.dbdjjuf.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5001;
+// const CONNEXION_URL = "mongodb+srv://massi:massi@cluster0.dbdjjuf.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process?.env?.PORT || 5001;
 
 
 mongoose.set('strictQuery', false)
-mongoose.connect(CONNEXION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process?.env?.CONNEXION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => {
     app.listen(PORT, console.log(`Server running on port: ${PORT}`.green.underline.italic))
   })
