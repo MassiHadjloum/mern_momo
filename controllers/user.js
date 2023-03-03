e70d0c93b6810bd3703dae77f60260bb76cad65d
MassiHadjloum <massihadjloum@gmail.com>
2023-03-03 11:20:07 +0100

    sign up function

--- controllers/user.js
+++ controllers/user.js
@@ -30,3 +30,24 @@ export const signInUser = async (req, res) => {
       res?.status(404).json({ message: 'User doesn\'t exist' })
     })
 }
+export const signUpUser = (req, res) => {
+  const { email, password, firstName, lastName, confirmPassword } = req?.body
+  User?.findOne({ email })
+    .then((user) => {
+      if (user) {
+        res.status(400).json({ message: 'User already exists.' })
+      } else if (password !== confirmPassword) {
+        res.status(400).json({ message: 'password don\'t match' })
+      } else {
+        const hashedPass = bcrypt.hash(password, 12).then((hashpass) => {
+          User.create({ email, password: hashpass, name: `${firstName} ${lastName}` })
+            .then((user) => {
+              const token = jwt.sign({ email: user?.email, id: user?._id }, 'test', { expiresIn: '1h' })
+              res.json(200).json({ data: { user, token } })
+            })
+            .catch((err) => res.json(400).json({ message: "Error: can't creat user" }))
+        }).catch((err) => res.json(400).json({ message: 'Error when crypting password' }))
+      }
+    })
+    .catch((err) => res.status(500).json({ message: 'something went wrong!!!' }))
+}
